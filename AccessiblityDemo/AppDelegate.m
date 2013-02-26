b449b9e729e4b1b01d6cd53a9ec87ead2dca09eb
David Rönnqvist <david.ronnqvist@gmail.com>
2013-02-26 21:54:28 +0100

    Changed AppDelegeate to set up app using slide in controller with left pane and main view

--- AccessiblityDemo/AppDelegate.m
+++ AccessiblityDemo/AppDelegate.m
@@ -8,7 +8,9 @@
 
 #import "AppDelegate.h"
 
-#import "ViewController.h"
+#import "SlideInController.h"
+#import "LeftPaneTableViewController.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
@@ -16,37 +18,17 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+    
+    SlideInController *slideInController = [SlideInController new];
+    slideInController.view.frame = [[UIScreen mainScreen] applicationFrame];
+    
+    slideInController.mainController = [MainViewController new];
+    slideInController.slidingController = [LeftPaneTableViewController new];
+    
+    self.window.rootViewController = slideInController;
     [self.window makeKeyAndVisible];
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
