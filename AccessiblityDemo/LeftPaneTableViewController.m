241432f28f986c5e0709e784d7718188612d486a
David Rönnqvist <david.ronnqvist@gmail.com>
2013-03-01 18:00:19 +0100

    Removed commented out template code of make demo less bloated and thus easier to read

--- AccessiblityDemo/LeftPaneTableViewController.m
+++ AccessiblityDemo/LeftPaneTableViewController.m
@@ -94,45 +94,6 @@ static NSString * const kPaletteCellIdentifier = @"PaletteCellIdentifier";
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
@@ -178,8 +139,6 @@ static NSString * const kPaletteCellIdentifier = @"PaletteCellIdentifier";
         [palettes addObject:[ColorPalette colorPaletteWithGradientWithName:@"Love" fromColor:[UIColor colorWithRed:0.642 green:0.007 blue:0.000 alpha:1.000] toColor:[UIColor colorWithRed:0.992 green:0.440 blue:1.000 alpha:1.000] inNumberOfSteps:8]];
         
     }
-    
-    
     
     return [palettes copy];
 }
