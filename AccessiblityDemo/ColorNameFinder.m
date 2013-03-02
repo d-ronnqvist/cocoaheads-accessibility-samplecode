7d0b8e2855393cfe4ce9661d762247b2348f1fbd
David Rönnqvist <david.ronnqvist@gmail.com>
2013-03-02 18:45:38 +0100

    Updated license of all source code to MIT

--- AccessiblityDemo/ColorNameFinder.m
+++ AccessiblityDemo/ColorNameFinder.m
@@ -3,8 +3,23 @@
 //  AccessiblityDemo
 //
 //  Created by David Rönnqvist on 3/2/13.
-//  Copyright (c) 2013 David Rönnqvist. All rights reserved.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in the
+// Software without restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "ColorNameFinder.h"
 
@@ -37,7 +52,7 @@ static NSArray *allColorNames;
 
 @implementation ColorNameFinder
 
-// From Dave DeLong
+// This method comes from an answer by Dave DeLong on StackOverflow
 // url: http://stackoverflow.com/a/3805354/608157
 void SKScanHexColor(NSString * hexString, float * red, float * green, float * blue, float * alpha) {
     NSString *cleanString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
